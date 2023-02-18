aa3c842dfe0177aab54bde8755054626d365e983
Pear107 <84367253+Pear107@users.noreply.github.com>
2023-02-18 09:38:22 +0800

    :art::art::art:

--- src/server/matchine_http_handler.cpp
+++
@@ -1,7 +0,0 @@
-#include "matchine_http_handler.h"
-#include "utils.h"
-
-void MatchineHttpHandler::handleRequest(Poco::Net::HTTPServerRequest& req, Poco::Net::HTTPServerResponse& res) {
-	const std::string body = utils::body("api is error");
-	res.sendBuffer(body.c_str(), body.length());
-}
